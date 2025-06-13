262b1375554d18a181afc637ef28a052eedfa444
akingo55 <am1157154@gmail.com>
2025-06-13 18:53:13 +0900

    udpate initial install script

--- initial_install.sh
+++ initial_install.sh
@@ -4,19 +4,21 @@ echo "start initial settings..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew update
 
-# copy
-cp .vimrc ~/.vimrc
-cp .gitconfig ~/.gitconfig
-cp .zshrc ~/.zshrc
-
 MAIN_SHELL=".zshrc"
 
-
+# peco
 brew install ghq peco
 cat peco.conf >> ~/.zshrc
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# tree icon
+# https://github.com/ryanoasis/nerd-fonts
+brew install font-hack-nerd-font
+
+#curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+#    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# neovim
+brew install neovim
 
 # python
 brew install pyenv
